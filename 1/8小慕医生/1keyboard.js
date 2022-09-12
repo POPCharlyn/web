2099ab8886f1f9210a7bce6acb4c3c389f7d4035
POPCharlyn <1540912102@qq.com>
2022-09-12 23:14:16 +0800

    Initial commit

---
+++ 1/8小慕医生/1keyboard.js
@@ -0,0 +1,4 @@
+export const upper={
+    console.log('you are the best in the worldxnasjkdnqwjkdhn!');qio;wsdhioqwdjqiowdjqopwdjqop[wsd[1    qwqwp;[d[   p1qwkd
+        [q2p-efdlfnjkbsdfkzdv ,;'dfvl;edfepllllll']]]]]
+}
---
+++ 1/temp.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b ='abc';
+for(var i =0;i<10;i++){
+    console.log(i);
+}
+
+function sum(){
+    while(1){
+        console.log('while');
+    }
+}
